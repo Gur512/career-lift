19b7df33b65f1c75ad7cf67af47fbadf9c1587e1
Gurpreet Kaur <gromana2022@gmail.com>
2024-12-13 05:50:49 +0530

    Adding style

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@
 
 import * as utils from "./utils.js";
 
+const loginBtn = utils.select('.login-btn');
+const emailInput = utils.select('.email-input');
+const passwordInput = utils.select('.password-input');
+
